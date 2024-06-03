240a7eb4200ebe63b9895170e9e3a0b87bef6a6b
geneus-solutions <geneus.solutionsit@gmail.com>
2024-06-03 07:04:54 +0530

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+import { Navigate, Router, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Navbar from './components/Navbar';
+import PageNotFound from './components/PageNotFound';
+import Product from './components/Product';
+import ProductDetails from './components/ProductDetails';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/product" element={<Product />} />
+          <Route path="/product/:id" element={<ProductDetails />} />
+          <Route path="/home" element={<Navigate to="/" />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </>
+    </Router>
   );
 }
 
